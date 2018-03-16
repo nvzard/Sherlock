1d1cdefcddded5bf039a0786baa9b8ca1f2b86a8
Nitanshu <nitanshu.vzard@gmail.com>
2018-03-16 23:39:13 +0530

    Added exit on no args

--- checker.py
+++ checker.py
@@ -5,23 +5,35 @@ import argparse
 import logging
 import glob
 
+from urllib.request import urlopen
 from packages import helpers
 
-
-
+OPTIONS = {
+            "l": "c",
+            "m": 10,
+            "d": 0,
+            "x": 0,
+            "c": "",
+            "n": 250
+            }
 
 def parse_args():
     parser = argparse.ArgumentParser(
-                    description = 'Plagiarism Checker')
+                    description = 'Plagiarism Checker.')
+    parser.add_argument(
+            '-l',
+            type=str,
+            help='Name of language.'
+            ),
     parser.add_argument(
             '-d',
             type=str,
-            help='directory containing files to be checked'
+            help='Directory containing files to be checked.'
             )
     parser.add_argument(
             '-u',
             type=int,
-            help ='user id given by moss'
+            help ='MOSS user id.'
             )
     return parser
 
@@ -33,9 +45,12 @@ def verify_args(args):
     if not args.u:
         logging.error('Missing user id(-u)')
         exit()
-    if not args.d:
+    elif not args.d:
         logging.error('Input location of dir containing files to be '
                       'checked using (-d)')
+        exit()
+    elif not args.l:
+        logging.error('Language name missing (-l)')
         exit()
 
 def abs_path(path):
@@ -53,24 +68,76 @@ def grab_files(path):
         logging.error('No files found in "{}"'.format(path))
         exit()
 
-def main(userid, path):
+def upload(link, files):
+    index = 0
+    for (name, path) in files:
+        size = os.path.getsize(path)
+        msg = 'file {0} {1} {2} {3}\n'.format(
+            index,
+            OPTIONS['l'],
+            size,
+            name
+        )
+        link.send(msg.encode())
+        print('Uploading {} ...'.format(name))
+        data = open(path, 'rb').read(size)
+        link.send(data)
+        index += 1
+    print('Upload complete')
+
+def transfer(userid, files, language):
+    server = 'moss.stanford.edu'
+    port = 7690
+    link = socket.socket()
+    link.connect((server, port))
+    print('Connection open')
+
+    link.send('moss {}\n'.format(userid).encode())
+    link.send("directory {}\n".format(OPTIONS['d']).encode())
+    link.send("X {}\n".format(OPTIONS['x']).encode())
+    link.send("maxmatches {}\n".format(OPTIONS['m']).encode())
+    link.send("show {}\n".format(OPTIONS['n']).encode())
+
+    link.send("language {}\n".format(language).encode())
+
+    status = link.recv(1024)
+
+    if status == 'no':
+        link.send(b'end\n')
+        link.close()
+        logging.error("Language not compatible by MOSS")
+        exit()
+
+    upload(link, files)
+
+    link.send('query 0 {}\n'.format(OPTIONS['c']).encode())
+    status = link.recv(1024)
+    link.send(b'end\n')
+    print('Closing link')
+    link.close()
+
+    return status.decode().replace('\n', '')
+
+def main(userid, path, language):
     files = grab_files(path)
-    print(files)
+    url = transfer(userid, files, language)
+    print(url)
 
 def command_line():
     parser = parse_args()
     args = parser.parse_args()
+    if len(sys.argv)==1:
+        parser.print_help()
+        exit()
     verify_args(args)
     USER_ID = args.u
-    print(USER_ID)
-    print(args.d)
     LOCATION = abs_path(args.d)
     if not check_path(LOCATION):
         logging.error('Directory "{}" does not exit!'.format(args.d))
         exit()
-    print(LOCATION)
-    main(USER_ID, LOCATION)
+    LANGUAGE = args.l
+    main(USER_ID, LOCATION, LANGUAGE)
 
-
-command_line()
+if __name__ == '__main__':
+    command_line()
 
